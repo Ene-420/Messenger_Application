abef04607af789f455f9630c2e47feb17e764cc6
Ene-420 <dimowoene@gmail.com>
2023-02-21 22:17:25 +0000

    Fragments Setup

--- app/src/main/java/com/example/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -2,13 +2,23 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
+
+
+    public void moveToRegisterPage(View view){
+        Intent moveToRegister = new Intent(getApplicationContext(), RegisterPageActivity.class);
+        startActivity(moveToRegister);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
+
+
     }
 }
--- app/src/main/java/com/example/myapplication/RegisterPageActivity.java
+++ app/src/main/java/com/example/myapplication/RegisterPageActivity.java
@@ -2,10 +2,17 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterPageActivity extends AppCompatActivity {
 
+
+    public void registerDetails(View view){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
